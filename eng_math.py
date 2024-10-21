79b896d7d8295282e8fc3de4f6f7258fac14322b
sunwoo <summerham22@gmail.com>
2024-10-21 22:45:26 +0900

    .

--- eng_math.py
+++ eng_math.py
@@ -3,15 +3,21 @@
 
 #영어점수, 수학점수
 score = [[56, 71], [80, 57], [91, 27], [14,67], [99, 67]]
-n = 5
 
-def solution(score, n):
-    eng_score = [eng for eng, math in score]
-    math_score = [math for eng, math in score]
+from itertools import combinations
 
-    eng_score.sort(reverse=True)
-    math_score.sort(reverse=True)
-    print(min(eng_score[:3]) + min(math_score[:3]))
-    return min(eng_score[:3]) + min(math_score[:3])
+def solution(score):
+    tmp_sum = 0
 
-solution(score, n)
+    for scores in combinations(score, 3):
+        min_eng = min([eng[0] for eng in scores])
+        min_math = min([math[1] for math in scores])
+
+        score_sum = min_eng + min_math
+        max_sum = max(tmp_sum, score_sum)
+    
+    print(max_sum)
+    return max_sum
+
+solution(score)
+
