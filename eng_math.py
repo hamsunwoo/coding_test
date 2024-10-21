1db622054ea7279837e4d25c7b8846b8f485d155
sunwoo <summerham22@gmail.com>
2024-10-21 21:24:53 +0900

    영어점수수학점수

---
+++ eng_math.py
@@ -0,0 +1,17 @@
+#N명 중 세명을 골랐을 때
+#영어점수 최솟값과 수학점수 최솟값의 합의 최대치를 반환
+
+#영어점수, 수학점수
+score = [[56, 71], [80, 57], [91, 27], [14,67], [99, 67]]
+n = 3
+
+def solution(score, n):
+    eng_score = [eng for eng, math in score]
+    math_score = [math for eng, math in score]
+
+    eng_score.sort(reverse=True)
+    math_score.sort(reverse=True)
+    print(min(eng_score[:n]) + min(math_score[:n]))
+    return min(eng_score[:n]) + min(math_score[:n])
+
+solution(score, n)
