41b7cced67d5a1e2f41db7c33422061adad37ff0
sunwoo <summerham22@gmail.com>
2024-10-21 22:20:53 +0900

    .

--- eng_math.py
+++ eng_math.py
@@ -3,7 +3,7 @@
 
 #영어점수, 수학점수
 score = [[56, 71], [80, 57], [91, 27], [14,67], [99, 67]]
-n = 3
+n = 5
 
 def solution(score, n):
     eng_score = [eng for eng, math in score]
@@ -11,7 +11,7 @@ def solution(score, n):
 
     eng_score.sort(reverse=True)
     math_score.sort(reverse=True)
-    print(min(eng_score[:n]) + min(math_score[:n]))
-    return min(eng_score[:n]) + min(math_score[:n])
+    print(min(eng_score[:3]) + min(math_score[:3]))
+    return min(eng_score[:3]) + min(math_score[:3])
 
 solution(score, n)
