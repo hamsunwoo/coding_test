ff53c8c3a633ba260121eed4a0e1b9dfaee104f8
sunwoo <summerham22@gmail.com>
2025-02-21 17:44:26 +0900

    bfs 타켓넘버

---
+++ programmers/dfs_bfs/타겟_넘버.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(numbers, target):
+    queue = deque([(0,0)])
+    answer = 0
+
+    while queue:
+        current_sum, idx = queue.popleft()
+
+        if idx == len(numbers):
+            if current_sum == target:
+                answer += 1
+
+
+        else:
+            queue.append((current_sum + numbers[idx], idx + 1))
+            queue.append((current_sum - numbers[idx], idx + 1))
+
+    return answer
