69d918dca8360f07860013d7c2f60961aed2d8c2
sunwoo <summerham22@gmail.com>
2025-03-13 13:08:49 +0900

    sql group by

---
+++ programmers/sql/조건에_맞는_사원_정보_조회하기.sql
@@ -0,0 +1,13 @@
+SELECT SUM(g.SCORE) AS SCORE,
+        g.EMP_NO,
+        e.EMP_NAME,
+        e.POSITION,
+        e.EMAIL
+FROM HR_EMPLOYEES e
+LEFT JOIN HR_GRADE g ON e.EMP_NO = g.EMP_NO
+GROUP BY g.EMP_NO,
+        e.EMP_NAME,
+        e.POSITION,
+        e.EMAIL
+ORDER BY SCORE DESC
+LIMIT 1;
