ef7b93dea2de266a5f428e5d8b0db3558ed68e7f
hamsunwoo <summerham22@gmail.com>
2025-10-16 15:51:54 +0900

    데이터 분석

---
+++ programmers/250121.py
@@ -0,0 +1,9 @@
+def solution(data, ext, val_ext, sort_by):
+    answer = []
+    dic = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
+    
+    for i in data:
+        if i[dic[ext]] < val_ext:
+            answer.append(i)
+
+    return sorted(answer, key=lambda x: x[dic[sort_by]])
