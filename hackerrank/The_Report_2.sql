e71940c56b42870594a1092df9f660eeed7f5940
hamsunwoo <summerham22@gmail.com>
2025-12-04 14:11:11 +0900

    The_Report 다시풀기

---
+++ hackerrank/The_Report_2.sql
@@ -0,0 +1,8 @@
+SELECT CASE WHEN g.Grade >= 8 THEN s.Name 
+            WHEN g.Grade < 8 THEN 'NULL'
+        END AS name,
+        g.Grade,
+        s.Marks
+FROM Students s
+JOIN Grades g ON s.Marks BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY g.Grade DESC, s.Name ASC;
