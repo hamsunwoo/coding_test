ed8c8e46be79b2a424159ede4fe7b8f2160ab0b9
hamsunwoo <summerham22@gmail.com>
2026-02-05 15:35:42 +0900

    primary-department-for-each-employee

---
+++ leetcode/primary-department-for-each-employee.sql
@@ -0,0 +1,16 @@
+SELECT employee_id,
+        department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id,
+        department_id
+FROM Employee
+WHERE primary_flag = 'N'
+AND employee_id NOT IN (
+    SELECT employee_id
+FROM Employee
+WHERE primary_flag = 'Y'
+)
